271b567bb9264e94506e72316e01e2a3bcb9bbd6
Max <mxlang@live.com>
2024-03-24 10:54:24 -0400

    Update functions using async/await to make more concise

--- index.js
+++ index.js
@@ -13,50 +13,36 @@ const detectlanguage = new DetectLanguage(API_KEY); // use DetectLanguage to cre
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function fetchData(word) {
-  return new Promise((resolve, reject) => {
-    const myPromise = detectlanguage.detect(word); // output: [object Promise]
-    console.log(`myPromise: ${myPromise}`);
+async function fetchData(word) {
+  try {
+    const detectionResult = await detectlanguage.detect(word);
+    const detectedLanguage = detectionResult[0].language;
+    const confidenceResult = detectionResult[0].confidence;
+    const translationResult = await translateLanguage(detectedLanguage);
+    const finalTranslation = translationResult;
 
-    myPromise.then((result) => {
-      console.log(`Result: ${JSON.stringify(result)}`); // 
-      console.log(`Result: ${result}`); // 
-
-      let finalResult = {
-        wordEval: result,
-        firstResult: result[0],
-        languageResult: result[0].language,
-        confidenceResult: result[0].confidence
-      };
-
-      let langugageTranslation = translateLanguage(finalResult.languageResult); // returns a Promise object
-      console.log(`this is the languageTranslation variable: ${langugageTranslation}`);
-
-      resolve(result);
-    }).catch((error) => {
-      console.error(`Error: ${error}`);
-      reject(error);
-    });
-  });
+    const langObj = {
+      detectedLanguage: finalTranslation,
+      confidenceResult: confidenceResult
+    }
+    return langObj;
+  } catch (error) {
+    throw error;
+  }
 }
 
-function translateLanguage(language) {
-  JSON.stringify(language);
-
-  return new Promise((resolve, reject) => {
-    detectlanguage.languages().then(function(result) {
-      const matchingLanguage = result.find(obj => obj.code === language);
-      if (matchingLanguage) {
-        console.log(`The language is: ${matchingLanguage.name}`);
-        resolve(language);
-      } else {
-        reject("No matching language was found in list. Promise rejected.");
-      }
-    }).catch(error => {
-      console.error("Error occurred:", error);
-      reject(error);
-    });
-  })
+async function translateLanguage(languageCode) {
+  try {
+    const languageArray = await detectlanguage.languages();
+    const matchingLanguage = languageArray.find(obj => obj.code === languageCode);
+    if (matchingLanguage) {
+      console.log(`The language is: ${matchingLanguage.name}`);
+    }
+    return matchingLanguage.name;
+  }    
+  catch (error) {
+    throw error;
+  }
 };
 
 app.get("/", (req, res) => {
@@ -67,21 +53,19 @@ app.get("/", (req, res) => {
   }
 });
 
-
 app.post("/detect-language", async (req, res) => {
-  let postBody = req.body['user-input']; // get user input from client side
-  fetchData(postBody)
-    .then((result) => {
-      let finalResult = {
-        wordEval: result,
-      };
-      res.render("index.ejs", {data: finalResult});
+  try {
+    let postBody = req.body['user-input']; // get user input from client side
+    const languageObject = await fetchData(postBody);
+    console.log(languageObject);
 
-    }).catch((error) => {
-      console.error(`Error occurred: ${error}`);
+    res.render("index.ejs", {data: languageObject});
+  }
+  catch (error) {
       res.status(500).send("An error occurred"); // Send an error response if promise is rejected
-    });
-  });
+      throw error;
+  }
+});
 
 app.listen(port, () => {
   console.log(`Successfully listening on port: ${port}`);
